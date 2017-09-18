d868ac33edebb94f81a259af2c3eca311aa23d53
Mtk112 <impanaza@hotmail.com>
2017-09-18 12:10:03 +0300

    Joujou

--- app/src/main/java/com/example/miikka/calorieswatcher/MainActivity.java
+++ app/src/main/java/com/example/miikka/calorieswatcher/MainActivity.java
@@ -3,11 +3,16 @@ package com.example.miikka.calorieswatcher;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import Fragments.Histograph;
+
 public class MainActivity extends AppCompatActivity {
 //jottain
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    Histograph hFrag = new Histograph();
+    getSupportFragmentManager().beginTransaction().replace(R.id.fragTemplate, hFrag).commit();
+
+}
 }
