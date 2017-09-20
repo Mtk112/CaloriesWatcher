e40cb82be0702487dd3dc3888eb11d3b1161e1b9
Mtk112 <impanaza@hotmail.com>
2017-09-20 19:44:32 +0300

    Added Exercise table to the Database, updated Dummy Data method.

--- app/src/main/java/Fragments/Histograph.java
+++ app/src/main/java/Fragments/Histograph.java
@@ -1,7 +1,5 @@
 package Fragments;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,14 +8,6 @@ import android.view.ViewGroup;
 
 import com.example.miikka.calorieswatcher.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link Histograph.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link Histograph#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Histograph extends Fragment {
 
 
